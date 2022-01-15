c5d4f6b056d921a3ffb286f29a06e57dde3c53f4
Tomer Libal <shaolintl@gmail.com>
2022-01-15 18:00:29 +0100

    lab 01 ready

--- lab01/tests/q0.py
+++ lab01/tests/q0.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q0',
     'points': None,
-    'suites': [{'cases': [{'code': '>>> secret_word == "welcome"\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> len(partners) > 0 \nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
